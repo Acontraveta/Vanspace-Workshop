29d105f84fadc0c53a0d4301474049067880d3da
Daniel Gutierrez <acontravetaes@gmail.com>
2026-02-09 17:21:18 +0100

    Fix TypeScript errors for deployment

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './app/App'
 import './index.css'
-import { AuthProvider } from './providers/AuthProvider'
-import { QueryProvider } from './providers/QueryProvider'
-import { NotificationProvider } from './providers/NotificationProvider'
+import { AuthProvider } from './app/providers/AuthProvider'
+import { QueryProvider } from './app/providers/QueryProvider'
+import { NotificationProvider } from './app/providers/NotificationProvider'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
