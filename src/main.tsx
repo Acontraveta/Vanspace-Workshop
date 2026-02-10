c38fc1d9814b0a6d9e16019bd2667633fb2812da
Daniel Gutierrez <acontravetaes@gmail.com>
2026-02-10 08:50:48 +0100

    Force rebuild: add clarifying comment to index.css import

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app/App'
-import '../index.css'
+import '../index.css' // CSS file in root directory
 import { AuthProvider } from './app/providers/AuthProvider'
 import { QueryProvider } from './app/providers/QueryProvider'
 import { NotificationProvider } from './app/providers/NotificationProvider'
