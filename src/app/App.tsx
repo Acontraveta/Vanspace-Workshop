36e4b127f9d0494f1a0c41a426a4140eaa7280db
Daniel Gutierrez <acontravetaes@gmail.com>
2026-02-26 11:00:59 +0100

    fix: error insertBefore en Node  providers duplicados
    App.tsx envolvía de nuevo en QueryProvider/AuthProvider/NotificationProvider
    que ya estaban en main.tsx. Esto creaba dos Toaster y dos AuthContext
    manipulando el DOM simultáneamente, causando el error de insertBefore.
    
    Ahora App.tsx solo renderiza RouterProvider.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,6 @@
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
-import { QueryProvider } from './providers/QueryProvider'
-import { AuthProvider } from './providers/AuthProvider'
-import { NotificationProvider } from './providers/NotificationProvider'
 
 export default function App() {
-  return (
-    <QueryProvider>
-      <AuthProvider>
-        <NotificationProvider>
-          <RouterProvider router={router} />
-        </NotificationProvider>
-      </AuthProvider>
-    </QueryProvider>
-  )
+  return <RouterProvider router={router} />
 }
